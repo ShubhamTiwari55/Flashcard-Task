bb10931f162d27f4c892dc929677f263e2159568
ShubhamTiwari55 <stofficial059@gmail.com>
2024-08-12 15:36:36 +0530

    added change

--- backend/server.js
+++ backend/server.js
@@ -58,5 +58,5 @@ app.delete('/api/flashcards/:id', (req, res) => {
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running on port 5000');
+  console.log(`Server is running on port ${PORT}`);
 });
