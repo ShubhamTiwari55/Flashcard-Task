22c1df3eff10714d81241a22ad6c047f8cb7014b
ShubhamTiwari55 <stofficial059@gmail.com>
2024-08-12 23:05:21 +0530

    added changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import axios from 'axios';
+
+// Set the base URL globally
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://3.110.175.178:5000';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
