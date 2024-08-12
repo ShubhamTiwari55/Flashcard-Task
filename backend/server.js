0359eb154d1bd03e52b56f2468651087016b7d08
ShubhamTiwari55 <stofficial059@gmail.com>
2024-08-12 21:37:55 +0530

    added sql changes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Database connection setup to remote server
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '3.110.175.178',  // Change this to the remote database IP address
   user: 'root',
   password: 'Shubham@559',
   database: 'flashcards_db',
@@ -21,15 +23,21 @@ db.connect((err) => {
   console.log('Connected to MySQL database.');
 });
 
-// Get all flashcards
-app.get('/api/flashcards', (req, res) => {
+// Proxy setup for forwarding API requests
+app.use('/api', createProxyMiddleware({
+  target: 'http://3.110.175.178:5000',  // Remote API server
+  changeOrigin: true,
+}));
+
+// Get all flashcards (this route still operates locally)
+app.get('/', (req, res) => {
   db.query('SELECT * FROM flashcards', (err, result) => {
     if (err) throw err;
     res.send(result);
   });
 });
 
-// Add a new flashcard
+// Add a new flashcard (this will also operate locally)
 app.post('/api/flashcards', (req, res) => {
   const { question, answer } = req.body;
   db.query('INSERT INTO flashcards (question, answer) VALUES (?, ?)', [question, answer], (err, result) => {
@@ -56,7 +64,9 @@ app.delete('/api/flashcards/:id', (req, res) => {
     res.send('Flashcard deleted!');
   });
 });
+
+// Start the server on port 5000 or a custom port defined in the environment variables
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running on port 5000');
+  console.log(`Server is running on port ${PORT}`);
 });
